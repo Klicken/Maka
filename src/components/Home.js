7ef601d2a6c776548ef1af9bd242ac54e481e9ad
martin karlsson <martin.dif@hotmail.com>
2019-11-10 14:59:42 +0100

    homepage fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { makeStyles, Typography } from "@material-ui/core";
+import { makeStyles, Typography, Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   message: {
     textAlign: "center",
+    marginBottom: "20px"
+  },
+  links: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center"
+  },
+  link: {
+    margin: "5px",
   }
 });
 
@@ -12,7 +21,44 @@ export default function Home() {
 
   return (
     <div>
-        <Typography variant="h5" className={classes.message}>This homepage needs something huh</Typography>
+      <Typography variant="h3" className={classes.message}>
+        HEY!
+      </Typography>
+      <Typography variant="h5" className={classes.message}>
+        Welcome to my site, this homepage needs something huh
+      </Typography>
+      <Typography variant="subtitle1" className={classes.message}>
+        Meanwhile, checkout the other links
+      </Typography>
+      <div className={classes.links}>
+          <Button
+            href="/blog"
+            size="small"
+            color="inherit"
+            variant="outlined"
+            className={classes.link}
+          >
+            Blog
+          </Button>
+          <Button
+            href="/about"
+            size="small"
+            color="inherit"
+            variant="outlined"
+            className={classes.link}
+          >
+            About
+          </Button>
+          <Button
+            href="/contact"
+            size="small"
+            color="inherit"
+            variant="outlined"
+            className={classes.link}
+          >
+            Contact
+          </Button>
+        </div>
     </div>
   );
 }
